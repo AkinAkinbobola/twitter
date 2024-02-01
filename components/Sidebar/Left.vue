a8aab2f60f9af38404326d6b53cb1fe071c90866
Akin Akinbobola <akinbobola71@gmail.com>
2024-02-01 14:24:36 +0100

    Add logo

---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>
+  <LogoTwitter/>
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
-<script setup lang="ts">
-
+<script setup>
+const darkMode = ref(false)
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div :class="{'dark': darkMode}">
+    <div class="min-h-full">
+      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl
+      lg:px-8 lg:gap-5">
+        <!--        Left-->
+        <div class="bg-red-400 md:block xs:col-span-1 xl:col-span-2">
+          <div
+          class="sticky top-0">
+            <SidebarLeft/>
+          </div>
+        </div>
+
+        <!--        Main-->
+        <div>
+
+        </div>
+
+        <!--        Right-->
+        <div>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
