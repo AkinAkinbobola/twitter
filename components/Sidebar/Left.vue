10e5769cf5fef28462ef55cff8163f8330d93573
Akin Akinbobola <akinbobola71@gmail.com>
2024-02-12 11:57:51 +0100

    Add logo transition

--- components/Sidebar/Left.vue
+++ components/Sidebar/Left.vue
@@ -1,11 +1,15 @@
 <script setup>
-
+const transition = 'transition ease-in-out duration-350'
 </script>
 
 <template>
-  <div class="p-2 my-2 w-min hover:rounded-full hover:bg-blue-50 dark:hover:bg-white/20">
-    <NuxtLink to="/">
-      <Icon name="mdi:twitter" class="text-blue-400 dark:text-white h-8 w-8"/>
-    </NuxtLink>
+  <div class="h-screen flex flex-col">
+    <div class="p-2 my-2 w-min rounded-full hover:bg-blue-50 dark:hover:bg-white/20"
+         :class="transition"
+    >
+      <NuxtLink to="/">
+        <Icon name="mdi:twitter" class="text-blue-400 dark:text-white h-8 w-8"/>
+      </NuxtLink>
+    </div>
   </div>
 </template>
