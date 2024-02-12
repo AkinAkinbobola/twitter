5a38991223eecad403d69b2927e75815f5155901
Akin Akinbobola <akinbobola71@gmail.com>
2024-02-12 11:48:49 +0100

    Add logo and dark mode

--- components/Sidebar/Left.vue
+++ components/Sidebar/Left.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-<div>
-  <Icon name="mdi:twitter" class="text-blue-300"/>
-</div>
+  <div class="p-2 my-2 w-min hover:rounded-full hover:bg-blue-50 dark:hover:bg-white/20">
+    <NuxtLink to="/">
+      <Icon name="mdi:twitter" class="text-blue-400 dark:text-white h-8 w-8"/>
+    </NuxtLink>
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,27 +4,29 @@ const darkMode = ref(false)
 
 <template>
   <div :class="{'dark': darkMode}">
-    <div class="min-h-full">
-      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl
       lg:px-8 lg:gap-5">
-        <!--        Left-->
-        <div class="bg-red-400 md:block xs:col-span-1 xl:col-span-2">
-          <div
-          class="sticky top-0">
-            <SidebarLeft/>
+          <!--        Left-->
+          <div class="md:block xs-col-span-1 xl:col-span-2">
+            <div
+                class="sticky top-0">
+              <SidebarLeft class="cursor-pointer"/>
+            </div>
           </div>
+
+          <!--        Main-->
+          <div>
+
+          </div>
+
+          <!--        Right-->
+          <div>
+
+          </div>
+
         </div>
-
-        <!--        Main-->
-        <div>
-
-        </div>
-
-        <!--        Right-->
-        <div>
-
-        </div>
-
       </div>
     </div>
   </div>
