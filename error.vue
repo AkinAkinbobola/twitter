53f11117b43a9a328abaa2239f1d448dcf45624c
Akin Akinbobola <65359899+AkinAkinbobola@users.noreply.github.com>
2024-02-26 12:03:20 +0100

    Update error.vue

--- error.vue
+++ error.vue
@@ -6,6 +6,6 @@
   <div class="flex h-screen justify-center items-center flex-col">
     <h1 class="text-9xl">404</h1>
     <p class="m-7 text-4xl">Page not found</p>
-    <button @click="navigateTo('/')">Go Back Home</button>
+    <button @click="navigateTo('/')" class="rounded text-2xl bg-blue-400 px-7 py-4 text-white">Go Back Home</button>
   </div>
 </template>
