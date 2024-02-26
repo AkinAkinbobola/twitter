e4e63316cb0925eea6f47b1bec6196418d2fc39c
Akin Akinbobola <65359899+AkinAkinbobola@users.noreply.github.com>
2024-02-26 12:08:54 +0100

    Add useError composable

--- error.vue
+++ error.vue
@@ -1,11 +1,11 @@
 <script setup>
-
+const error = useError()
 </script>
 
 <template>
   <div class="flex h-screen justify-center items-center flex-col">
-    <h1 class="text-9xl">404</h1>
-    <p class="m-7 text-4xl">Page not found</p>
+    <h1 class="text-9xl">{{ error.statusCode }}</h1>
+    <p class="m-7 text-4xl">{{ error.statusMessage }}</p>
     <button @click="navigateTo('/')" class="rounded text-2xl bg-blue-400 px-7 py-4 text-white">Go Back Home</button>
   </div>
 </template>
